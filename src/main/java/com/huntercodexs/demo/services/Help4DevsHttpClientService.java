e4306e40587a5dfe786bd71176e8b6cec141ce8a
Jereelton Teixeira <jereeltondevel@gmail.com>
2024-04-12 00:00:24 -0300

    feat: [stabled] [work] [tested] Added httpHeadersCreate + tests

--- src/main/java/com/huntercodexs/demo/services/Help4DevsHttpClientService.java
+++ src/main/java/com/huntercodexs/demo/services/Help4DevsHttpClientService.java
@@ -1,10 +1,25 @@
 package com.huntercodexs.demo.services;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.LinkedHashMap;
+import java.util.List;
 
 @Slf4j
 @Service
 public class Help4DevsHttpClientService {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    private static HttpHeaders httpHeadersCreate(List<LinkedHashMap<String, String>> headers) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        for (LinkedHashMap<String, String> header : headers) {
+            httpHeaders.add(header.get("header"), header.get("value"));
+        }
+        return httpHeaders;
+    }
 
 }
