26d6e56f046b4d4dd9747c2bb6c5a22bd8004e57
Hunter Codexs <huntercodexs@gmail.com>
2024-04-27 00:41:26 -0300

    feat: [stabled] [work] [tested] Added functions from HackerRank Challenge

--- src/main/java/com/huntercodexs/demo/services/Help4DevsHackerRankService.java
+++ src/main/java/com/huntercodexs/demo/services/Help4DevsHackerRankService.java
@@ -3,11 +3,186 @@ package com.huntercodexs.demo.services;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+
 @Slf4j
 @Service
 public class Help4DevsHackerRankService {
 
-    public static boolean isPangram(String input, String language) {
+    public static void plusMinus(List<Integer> arr) {
+
+        int positive = 0;
+        int negative = 0;
+        int zero = 0;
+        int arrSize = arr.size();
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) > 0) positive += 1;
+            if (arr.get(i) < 0) negative += 1;
+            if (arr.get(i) == 0) zero += 1;
+        }
+
+        float resultPositive = (float) positive / (float) arrSize;
+        float resultNegative = (float) negative / (float) arrSize;
+        float resultZeros = (float) zero / (float) arrSize;
+
+        System.out.printf("%f\n", resultPositive);
+        System.out.printf("%f\n", resultNegative);
+        System.out.printf("%f\n", resultZeros);
+
+    }
+
+    public static void miniMaxSum(List<Integer> arr) {
+
+        int arrSize = arr.size();
+        long minSum = 0;
+        long maxSum = 0;
+
+        arr.sort(Comparator.naturalOrder());
+
+        for (int i = 0; i < arrSize-1; i++) {
+            minSum += arr.get(i);
+            maxSum += arr.get(i+1);
+        }
+
+        System.out.println(minSum + " " + maxSum);
+    }
+
+    public static String timeConversion(String s) {
+
+        String result = null;
+        int militaryHour = 12;
+        String[] matches = s.replaceAll("(PM|AM)$", "").split(":");
+
+        if (s.matches("(.*)PM$")) {
+
+            if (!matches[0].equals("12")) {
+                militaryHour = Integer.parseInt(matches[0]) + 12;
+            }
+            result = militaryHour +":"+ matches[1] +":"+ matches[2];
+
+        } else if (s.matches("(.*)AM")) {
+
+            if (matches[0].equals("12")) {
+                matches[0] = "00";
+            }
+            result = matches[0] +":"+ matches[1] +":"+ matches[2];
+        }
+
+        return result;
+    }
+
+    public static List<Integer> matchingStrings(List<String> strings, List<String> queries) {
+
+        List<Integer> matches = new ArrayList<>();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        queries.forEach(query -> {
+
+            hashMap.put(query, 0);
+
+            for (int i = 0; i < strings.size(); i++) {
+
+                if (query.equals(strings.get(i))) {
+                    int counter = 0;
+                    if (hashMap.get(query) != null) {
+                        counter += hashMap.get(query);
+                    }
+                    hashMap.put(query, counter+1);
+                }
+
+            }
+
+            matches.add(hashMap.get(query));
+
+        });
+
+        return matches;
+
+    }
+
+    public static int lonelyinteger(List<Integer> a) {
+
+        int unique = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            unique =  unique ^ a.get(i);
+        }
+
+        return unique;
+
+    }
+
+    public static long flippingBits(long n) {
+
+        String bits = String.format("%32s", Long.toBinaryString(n)).replace(" ", "0");
+        bits = bits.replaceAll("1", "2");
+        bits = bits.replaceAll("0", "1");
+        bits = bits.replaceAll("2", "0");
+        long res = Long.parseLong(bits, 2);
+
+        //System.out.println(res);
+
+        return res;
+
+    }
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+
+        List<Integer> leftToRight = new ArrayList<>();
+        List<Integer> rightToLeft = new ArrayList<>();
+
+        int indexLeft = 0;
+        int indexRight = arr.size() - 1;
+
+        for (int i = 0; i < arr.size(); i++) {
+            leftToRight.add(arr.get(i).get(indexLeft));
+            indexLeft+=1;
+            rightToLeft.add(arr.get(i).get(indexRight));
+            indexRight-=1;
+        }
+
+        int leftRightSum = 0;
+        int rightLeftSum = 0;
+
+        for (int itemL : leftToRight) {
+            leftRightSum += itemL;
+        }
+
+        for (int itemR : rightToLeft) {
+            rightLeftSum += itemR;
+        }
+
+        int result = Math.abs(leftRightSum-rightLeftSum);
+
+        System.out.println(result);
+
+        return result;
+    }
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+
+        List<Integer> resultSort = new ArrayList<>();
+
+        arr.sort(Comparator.naturalOrder());
+
+        for (int i = 0; i < 100; i++) {
+            int counter = 0;
+            for(int num : arr) {
+                if (num == i) {
+                    counter+=1;
+                }
+            }
+            resultSort.add(counter);
+        }
+
+        return resultSort;
+    }
+
+    public static boolean pangrams(String input, String language) {
 
         String[] englishAlphabet = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
 
--- src/test/java/codexstester/test/unitary/Help4DevsHackerRankUnitaryTests.java
+++ src/test/java/codexstester/test/unitary/Help4DevsHackerRankUnitaryTests.java
@@ -3,17 +3,252 @@ package codexstester.test.unitary;
 import codexstester.setup.bridge.Help4DevsBridgeTests;
 import org.junit.Test;
 
-import static com.huntercodexs.demo.services.Help4DevsHackerRankService.isPangram;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.huntercodexs.demo.services.Help4DevsHackerRankService.*;
+import static com.huntercodexs.demo.services.Help4DevsToolsService.stdout;
 
 public class Help4DevsHackerRankUnitaryTests extends Help4DevsBridgeTests {
+
+    @Test
+    public void plusMinusTest() {
+        List<Integer> list = new ArrayList<>();
+        list.add(-4);
+        list.add(3);
+        list.add(-9);
+        list.add(0);
+        list.add(4);
+        list.add(1);
+        plusMinus(list);
+    }
+
+    @Test
+    public void miniMaxSumTest() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        miniMaxSum(list);
+    }
+
+    @Test
+    public void timeConversionTest() {
+        String result = timeConversion("07:05:45PM");
+        codexsTesterAssertText("19:05:45", result);
+
+        result = timeConversion("07:05:45AM");
+        codexsTesterAssertText("07:05:45", result);
+
+        result = timeConversion("12:05:45PM");
+        codexsTesterAssertText("12:05:45", result);
+
+        result = timeConversion("12:05:45AM");
+        codexsTesterAssertText("00:05:45", result);
+    }
+
+    @Test
+    public void matchingStringsTest() {
+        List<String> source = new ArrayList<>();
+        List<String> queries = new ArrayList<>();
+
+        source.add("4");
+        source.add("aba");
+        source.add("baba");
+        source.add("aba");
+        source.add("xzxb");
+        source.add("3");
+        source.add("aba");
+        source.add("xzxb");
+        source.add("ab");
+
+        queries.add("xzxb");
+        queries.add("baba");
+        queries.add("bc");
+
+        List<Integer> result = matchingStrings(source, queries);
+        codexsTesterAssertText("[2, 1, 0]", result.toString());
+
+    }
+
+    @Test
+    public void lonelyintegerTest() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+        int result = lonelyinteger(list);
+        codexsTesterAssertInt(4, result);
+    }
+
+    @Test
+    public void flippingBitsTest() {
+        long result = flippingBits(3);
+        stdout(String.valueOf(result));
+
+        result = flippingBits(2147483647);
+        stdout(String.valueOf(result));
+
+        result = flippingBits(1);
+        stdout(String.valueOf(result));
+
+        result = flippingBits(0);
+        stdout(String.valueOf(result));
+    }
+
+    @Test
+    public void diagonalDifferenceTest() {
+        List<List<Integer>> arr = new ArrayList<>();
+
+        List<Integer> line1 = new ArrayList<>();
+        line1.add(1);
+        line1.add(2);
+        line1.add(3);
+
+        List<Integer> line2 = new ArrayList<>();
+        line2.add(4);
+        line2.add(5);
+        line2.add(6);
+
+        List<Integer> line3 = new ArrayList<>();
+        line3.add(9);
+        line3.add(8);
+        line3.add(9);
+
+        arr.add(line1);
+        arr.add(line2);
+        arr.add(line3);
+
+        int result = diagonalDifference(arr);
+        codexsTesterAssertInt(2, result);
+    }
+
+    @Test
+    public void countingSortTest() {
+        List<Integer> arr = new ArrayList<>();
+        arr.add(100);
+        arr.add(63);
+        arr.add(25);
+        arr.add(73);
+        arr.add(1);
+        arr.add(98);
+        arr.add(73);
+        arr.add(56);
+        arr.add(84);
+        arr.add(86);
+        arr.add(57);
+        arr.add(16);
+        arr.add(83);
+        arr.add(8);
+        arr.add(25);
+        arr.add(81);
+        arr.add(56);
+        arr.add(9);
+        arr.add(53);
+        arr.add(98);
+        arr.add(67);
+        arr.add(99);
+        arr.add(12);
+        arr.add(83);
+        arr.add(89);
+        arr.add(80);
+        arr.add(91);
+        arr.add(39);
+        arr.add(86);
+        arr.add(76);
+        arr.add(85);
+        arr.add(74);
+        arr.add(39);
+        arr.add(25);
+        arr.add(90);
+        arr.add(59);
+        arr.add(10);
+        arr.add(94);
+        arr.add(32);
+        arr.add(44);
+        arr.add(3);
+        arr.add(89);
+        arr.add(30);
+        arr.add(27);
+        arr.add(79);
+        arr.add(46);
+        arr.add(96);
+        arr.add(27);
+        arr.add(32);
+        arr.add(18);
+        arr.add(21);
+        arr.add(92);
+        arr.add(69);
+        arr.add(81);
+        arr.add(40);
+        arr.add(40);
+        arr.add(34);
+        arr.add(68);
+        arr.add(78);
+        arr.add(24);
+        arr.add(87);
+        arr.add(42);
+        arr.add(69);
+        arr.add(23);
+        arr.add(41);
+        arr.add(78);
+        arr.add(22);
+        arr.add(6);
+        arr.add(90);
+        arr.add(99);
+        arr.add(89);
+        arr.add(50);
+        arr.add(30);
+        arr.add(20);
+        arr.add(1);
+        arr.add(43);
+        arr.add(3);
+        arr.add(70);
+        arr.add(95);
+        arr.add(33);
+        arr.add(46);
+        arr.add(44);
+        arr.add(9);
+        arr.add(69);
+        arr.add(48);
+        arr.add(33);
+        arr.add(60);
+        arr.add(65);
+        arr.add(16);
+        arr.add(82);
+        arr.add(67);
+        arr.add(61);
+        arr.add(32);
+        arr.add(21);
+        arr.add(79);
+        arr.add(75);
+        arr.add(75);
+        arr.add(13);
+        arr.add(87);
+        arr.add(70);
+        arr.add(33);
+
+        List<Integer> result = countingSort(arr);
+
+        codexsTesterAssertText(
+                "0 2 0 2 0 0 1 0 1 2 1 0 1 1 0 0 2 0 1 0 1 2 1 1 1 3 0 2 0 0 2 0 3 3 1 0 0 0 0 2 2 1 1 1 2 0 2 0 1 0 1 0 0 1 0 0 2 1 0 1 1 1 0 1 0 1 0 2 1 3 2 0 0 2 1 2 1 0 2 2 1 2 1 2 1 1 2 2 0 3 2 1 1 0 1 1 1 0 2 2",
+                result.toString().replaceAll("[\\]\\[]", "").replaceAll(",", ""));
+
+    }
 
     @Test
     public void pangramsTest() {
         String pangram = ("We promptly judged antique ivory buckles for the next prize".toLowerCase());
         String notPangram = ("The promptly judged antique ivory buckles for the next prize").toLowerCase();
 
-        codexsTesterAssertBool(true, isPangram(pangram, "en"));
-        codexsTesterAssertBool(false, isPangram(notPangram, "en"));
+        codexsTesterAssertBool(true, pangrams(pangram, "en"));
+        codexsTesterAssertBool(false, pangrams(notPangram, "en"));
     }
 
 }
